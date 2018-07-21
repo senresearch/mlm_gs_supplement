963e57e15c4c6efd73629438792ed53bcde7c91c
Jane Liang <jane.w.liang@gmail.com>
2018-07-21 01:46:23 -0400

    Correct source file copied over

--- dosage.jl
+++ dosage.jl
@@ -1,24 +1,14 @@
-# Set up workers
-addprocs(4)
+# Load libraries and dependencies
 
-# Load source files.
+using DataFrames
+using Distributions
+using MultipleTesting
+using Gadfly
+@everywhere using Loess
 @everywhere include("dummyfun.jl")
 @everywhere include("krondiag.jl")
-@everywhere include("collins.jl")
-@everywhere include("power_crossprod.jl")
-@everywhere include("lambda.jl")
-# Load packages.
-@everywhere using DataFrames
-@everywhere using StatsBase
-@everywhere using Distributions
 
-# Modified version of the ls function.
-# X, Y, and Z are 2d arrays. X and Z should have the correct contrasts applied to them. 
-# interceptX and interceptZ are boolean flags indicating whether or not to include X and Z
-# intercepts (main effects). Default is true for both. 
-# var_shrink is a boolean flag indicating whether or not to apply variance shrinkage. Default false.
-# targetType is a character string ("A", "B", "C", or "D") indicating the target for variance shrinkage. 
-# Default is "C". See lambda.jl for more details. 
+# Function to do matrix linear models
 @everywhere function MLM(X::Array{Float64,2}, Y::Array{Float64,2}, Z::Array{Float64,2},
   interceptX::Bool=true, interceptZ::Bool=true, var_shrink::Bool=false, targetType::AbstractString="C")
   if interceptX==true # Include X intercept
@@ -48,14 +38,9 @@ addprocs(4)
   return result
 end
 
-# Calls MLM and does post-processing. 
-# X, Y, and Z are 2d arrays.  X and Z should have the correct contrasts applied to them. 
-# interceptX and interceptZ are boolean flags indicating whether or not to include X and Z
-# intercepts (main effects). Default is true for both. 
-# var_shrink is a boolean flag indicating whether or not to apply variance shrinkage. Default false.
-# targetType is a character string ("A", "B", "C", or "D") indicating the target for variance shrinkage. 
-# Default is "C". See lambda.jl for more details. 
-@everywhere function my_MLM(Y,X,Z, var_shrink::Bool=false, targetType::AbstractString="C")
+# Modified MLM function that back-estimates "left-out" sum contrasts and returns the interaction t-statistics. 
+# X and Z should be sum contrasts
+@everywhere function my_MLM(X, Y, Z, var_shrink::Bool=false, targetType::AbstractString="C")
   # Run MLM and get coefficient, variance, and sigma estimates
   out = MLM(X::Array{Float64,2}, Y::Array{Float64,2}, Z::Array{Float64,2}, true, true, var_shrink, targetType)
 
@@ -64,20 +49,42 @@ end
   varleft = inv(transpose([ones(size(X,1)) X])*[ones(size(X,1)) X])
   varright = transpose(ZTZ\(transpose((ZTZ\(transpose([ones(size(Z,1)) Z])*out[3]))*[ones(size(Z,1)) Z])))
 
-  #C = transpose(vcat(0, -ones(size(X,2))))
-  #coeffX = C*out[1]
-  #varX = transpose(krondiag(C*varleft*transpose(C), varright))
+  C = transpose(vcat(0, -ones(size(X,2))))
+  coeffX = C*out[1]
+  varX = transpose(krondiag(C*varleft*transpose(C), varright))
 
-  D = vcat(zeros(15),-ones(size(Z,2)-14))
+  D = vcat(0,-ones(size(Z,2)))
   coeffZ = out[1]*D
   varZ = transpose(krondiag(varleft, transpose(D)*varright*D))
 
-  #coeffXZ = C*out[1]*D
-  #varXZ = transpose(krondiag(C*varleft*transpose(C), transpose(D)*varright*D))
-  #allcoeffs = vcat(out[1], coeffX)
-  #allcoeffs = hcat(allcoeffs, vcat(coeffZ, coeffXZ))
-  #allvar = vcat(out[2], varX)
-  #allvar = hcat(allvar, vcat(varZ, varXZ))
+  coeffXZ = C*out[1]*D
+  varXZ = transpose(krondiag(C*varleft*transpose(C), transpose(D)*varright*D))
+  allcoeffs = vcat(out[1], coeffX)
+  allcoeffs = hcat(allcoeffs, vcat(coeffZ, coeffXZ))
+  allvar = vcat(out[2], varX)
+  allvar = hcat(allvar, vcat(varZ, varXZ))
+  tstats = allcoeffs./sqrt(allvar)
+
+  # Return just the interactions
+  return tstats[2:end,2:end]
+end
+
+# Modified MLM function that back-estimates "left-out" sum contrasts and returns the interaction t-statistics. 
+# This one is for the dosage encoding, i.e. it only needs to back-estimate the extra Z (mutant) contrast. 
+# X should be encoded for doses. 
+# Z should be a sum contrast
+@everywhere function my_MLM_dos(X, Y, Z, var_shrink::Bool=false, targetType::AbstractString="C")
+  # Run MLM and get coefficient, variance, and sigma estimates
+  out = MLM(X::Array{Float64,2}, Y::Array{Float64,2}, Z::Array{Float64,2}, false, true, var_shrink, targetType)
+
+  # Post-processing to get the sum contrasts to be interpretable.
+  ZTZ = transpose([ones(size(Z,1)) Z])*[ones(size(Z,1)) Z]
+  varleft = inv(transpose(X)*X)
+  varright = transpose(ZTZ\(transpose((ZTZ\(transpose([ones(size(Z,1)) Z])*out[3]))*[ones(size(Z,1)) Z])))
+
+  D = vcat(0,-ones(size(Z,2)))
+  coeffZ = out[1]*D
+  varZ = transpose(krondiag(varleft, transpose(D)*varright*D))
 
   allcoeffs = hcat(out[1], coeffZ)
   allvar = hcat(out[2], varZ)
@@ -85,20 +92,55 @@ end
   tstats = allcoeffs./sqrt(allvar)
 
   # Return just the interactions
-  return tstats[2:end,16:end]
+  return tstats[:,2:end]
 end
 
-# Gets permutation p-values. 
-# fun is a function to call on X, Y, and Z to get test statistics. 
-# nperms is the number of permutations to run. 
-# X, Y, and Z are 2d arrays.  X and Z should have the correct contrasts applied to them. 
-# file_name is a string of the file extension to which the "real" test statistics should be written. 
-# fun_args... are addition arguments to be passed into fun
-function perm_test(fun::Function, nperms::Int64, Y::Array{Float64,2}, X::Array{Float64,2}, Z::Array{Float64,2}, 
-  file_name::String, fun_args...)
+# Calculates S scores (Collins, et al) with variance flooring
+# X and Z should be encoded as treatment contrasts with no intercept.  
+@everywhere function my_Sscore_floor(allConds::Array{Float64, 2}, Y::Array{Float64, 2}, allClones::Array{Float64, 2})
+  S = Array(Float64, size(allConds,2), size(allClones,2))
+
+  # Over clones
+  mucont = Array(Float64, size(allClones,2)) # median(colony size for that clone)
+  for j=1:size(allClones,2)
+    thisclone = Y[:,allClones[:,j].==1]
+    mucont[j] = median(thisclone)
+  end
+
+  # Over both.
+  muexp = Array(Float64, size(allConds,2), size(allClones,2)) # mean(colony sizes for that clone and condition)
+  varexp = Array(Float64, size(allConds,2), size(allClones,2)) # var(colony sizes for that clone and condition)
+  nexp = Array(Float64, size(allConds,2), size(allClones,2)) #  num of measurements of colony sizes for that clone and condition
+  for i=1:size(allConds,2), j=1:size(allClones,2)
+    thisclonecond = Y[allConds[:,i].==1,allClones[:,j].==1]
+    muexp[i,j] = mean(thisclonecond)
+    varexp[i,j] = var(thisclonecond)
+    nexp[i,j] = length(thisclonecond)
+  end
+
+  # Control variance with lower bound. 
+  varcont_lower = (mucont * median(sqrt(varexp)./muexp)).^2
+  varcont = max(transpose(median(varexp, 1)), varcont_lower)
+
+  # Control sample size. 
+  ncont = median(nexp)
+
+  # Experimental variance with lower bound. 
+  sdexp_loess = loess(vec(muexp), vec(sqrt(varexp)))
+  varexp = reshape(max(vec(varexp), Loess.predict(sdexp_loess, vec(muexp)).^2), size(muexp))
+
+  # Calculate S score. 
+  svar = (varexp.*(nexp-1) .+ transpose(varcont.*(ncont-1))) ./ (nexp + ncont - 2)
+  S = (muexp.-transpose(mucont))./sqrt(svar./nexp.+svar./ncont)
+  return S
+end
+
+# Get permutation test p-values for a given function fun. 
+# Assumes that fun will return an array of test statistics corresponding to interactions only (no main effects)
+function perm_test(fun::Function, nperms::Int64, X::Array{Float64,2}, Y::Array{Float64,2}, Z::Array{Float64,2}, 
+  fun_args...)
   # Get the "real" test statistics and write to file_name.
-  test_stats = fun(Y, X, Z, fun_args...)
-  writecsv(file_name, test_stats)
+  test_stats = fun(X, Y, Z, fun_args...)
 
   # Permute the rows of Y. For each permutation, re-run the method to get test statistics. 
   println("Starting permutations.")
@@ -106,7 +148,7 @@ function perm_test(fun::Function, nperms::Int64, Y::Array{Float64,2}, X::Array{F
   @sync @parallel for j in 1:nperms
     row_idx = shuffle(collect(1:size(Y,1)))
     Yperms = Y[row_idx,:]
-    perms[:,j] = vec(fun(Yperms, X, Z, fun_args...))
+    perms[:,j] = vec(fun(X, Yperms, Z, fun_args...))
   end
 
   # Get permutation p-values. 
@@ -153,6 +195,14 @@ all_Y = [Y1, Y2, Y3, Y4, Y5, Y6]
 all_X = [X1, X2, X3, X4, X5, X6]
 all_Z = [Z1, Z2, Z3, Z4, Z5, Z6]
 
+# Concatenate condition and concentration in all Xs.
+for x in all_X
+  x[:Cond_Conc] = DataArray(String, size(x,1))
+  for i in 1:size(x,1)
+    x[:Cond_Conc][i] = string(x[:Condition][i]," ",x[:Concentration][i])
+  end
+end
+
 # Standardize Ys by subtracting median and dividing by IQR.
 all_Ystd = []
 for i in 1:length(all_Y)
@@ -162,90 +212,99 @@ for i in 1:length(all_Y)
   end
 end
 
-# Add spatial coefficients to Z. 
-plate_center = [16.5, 24.5]
-for i in 1:length(all_Z)
-  all_Z[i] = hcat(all_Z[i], power_crossprod(all_Z[i][:row]-plate_center[1], all_Z[i][:column]-plate_center[2], [1, 2, 3, 4]))
-end
 
-# Intercepts (sum contrasts) and slopes for X
+# Dosage-response slopes for X
 all_Xdos = []
 for i in 1:length(all_X)
   X = all_X[i]
-  # Sum contrasts for every condition. 
-  Xsumc = contr(X[[:Condition]], [:Condition], ["sum"])
-  Xsumc = convert(Array{Float64}, Xsumc)
-  # Hack to drop the -1s for the last level (for now)
-  Xsumc[Xsumc .== -1] = 0
 
-  # Pull out every condition (except the last one, for now)
-  all_conds = levels(X[:Condition])[1:(end-1)]
+  # Pull out every condition 
+  all_conds = levels(X[:Condition])
   # For each condition, pull out all of the concentrations
   concs = [X[:Concentration][cond .== X[:Condition]] for cond in all_conds]
   # Get the number of unique concentration levels found for each condition
   n_levels = [length(levels(conc)) for conc in concs]
 
-  # Now keep only the conditions that have more than 1 concentration level. 
-  all_conds = all_conds[n_levels .> 1]
-  concs = concs[n_levels .> 1]
-  n_levels = n_levels[n_levels .> 1]
-
-  # Get the mean of the unique ordinal numbers corresponding to levels for each condition. 
-  n_level_means = [mean(collect(1:n)) for n in n_levels]
   # Dictionary to keep track of order of SI prefixes
   dict = Dict('m' => 4, 'u' => 3, 'n' => 2, 'p' =>1)
   # Initialize empty array of slopes
   slopes = zeros(size(X, 1), length(all_conds))
 
   for i in 1:length(all_conds)
-  	# For every condition, split the concentration levels on "sec", "%", and spaces. 
-  	splits = [split(x, r"sec|%|\s", keep=false) for x in levels(concs[i])]
-  	# If there is only one split for each concentration level 
-    # or the second split for each level (presumably a unit) is the same for all
-  	if (all([length(spl) for spl in splits] .== 1) || all([spl[2] for spl in splits] .== splits[1][2]))
-  		# Check that if the first split is non-numeric, that it is high/low
-  		if (all([isnull(tryparse(Float64, spl[1])) for spl in splits]))
-  			if (all([spl[1] in ["high", "low"] for spl in splits]))
-  				idx = sortperm([spl[1] for spl in splits], rev=true)
-  			else 
-  				print(splits)
-  				error("You need to create a different case")
-  			end
-  		# Otherwise, sort on the first split, assuming that it is a numeric value
-  		else
-  			idx = sortperm([parse(Float64, spl[1]) for spl in splits])
-  		end
-  	# If the first character in each of the second splits is an SI prefixes
-  	# Sort first by SI prefix (as indicated by the dicationary) and then within each prefix. 
-  	elseif (all([spl[2][1] in ['m','u','n','p'] for spl in splits]))
-  		A = [[dict[spl[2][1]] for spl in splits] [parse(spl[1]) for spl in (splits)] collect(1:length(splits))]
-  		idx = sortrows(A, by=x->(x[1],x[2]))[:,3]
-  	# Raise an error if neither case was met. 
-  	else
-  		print(splits)
-  		error("You need to create a different case")
-  	end
-  	# Assign slopes for each condition. 
-  	for j in 1:length(levels(concs[i]))
-  		slopes[(X[:Condition] .== all_conds[i]) & (X[:Concentration] .== levels(concs[i])[idx][j]), i] = j - n_level_means[i]
-	end
+    if n_levels[i] == 1
+      slopes[(X[:Condition] .== all_conds[i]) & (X[:Concentration] .== levels(concs[i])[1]), i] = 1
+    else 
+      # For every condition, split the concentration levels on "sec", "%", and spaces. 
+      splits = [split(x, r"sec|%|\s", keep=false) for x in levels(concs[i])]
+      # If there is only one split for each concentration level 
+      # or the second split for each level (presumably a unit) is the same for all
+      if (all([length(spl) for spl in splits] .== 1) || all([spl[2] for spl in splits] .== splits[1][2]))
+        # Check that if the first split is non-numeric, that it is high/low
+        if (all([isnull(tryparse(Float64, spl[1])) for spl in splits]))
+          if (all([spl[1] in ["high", "low"] for spl in splits]))
+            idx = sortperm([spl[1] for spl in splits], rev=true)
+          else 
+            print(splits)
+            error("You need to create a different case")
+          end
+        # Otherwise, sort on the first split, assuming that it is a numeric value
+        else
+          idx = sortperm([parse(Float64, spl[1]) for spl in splits])
+        end
+
+      # If the first character in each of the second splits is an SI prefixes
+      # Sort first by SI prefix (as indicated by the dicationary) and then within each prefix. 
+      elseif (all([spl[2][1] in ['m','u','n','p'] for spl in splits]))
+        A = [[dict[spl[2][1]] for spl in splits] [parse(spl[1]) for spl in (splits)] collect(1:length(splits))]
+        idx = sortrows(A, by=x->(x[1],x[2]))[:,3]
+      # Raise an error if neither case was met. 
+
+      else
+        print(splits)
+        error("You need to create a different case")
+      end
+
+      # Assign slopes for each condition. 
+      for j in 1:length(levels(concs[i]))
+        slopes[(X[:Condition] .== all_conds[i]) & (X[:Concentration] .== levels(concs[i])[idx][j]), i] = j
+      end
+    end
   end
-  push!(all_Xdos, hcat(Xsumc, slopes))
+  push!(all_Xdos, slopes)
 end
 
-# Sum contrasts for Z, including spatial effects 
+
+# Sum contrasts for X (condition-Concentration categorical encoding)
+all_Xsumc = []
+for i in 1:length(all_X)
+  push!(all_Xsumc, convert(Array{Float64}, contr(all_X[i][[:Cond_Conc]], [:Cond_Conc], ["sum"])))
+end
+
+# Sum contrasts for X (condition categorical encoding) 
+all_Xsumc_cond = []
+for i in 1:length(all_X)
+  push!(all_Xsumc_cond, convert(Array{Float64}, contr(all_X[i][[:Condition]], [:Condition], ["sum"])))
+end
+
+# Sum contrasts for Z
 all_Zsumc = []
 for i in 1:length(all_Z)
-  push!(all_Zsumc, convert(Array{Float64}, contr(all_Z[i][vcat(names(all_Z[i])[9:end], :name)], [:name], ["sum"])))
+  push!(all_Zsumc, convert(Array{Float64}, contr(all_Z[i][[:name]], [:name], ["sum"])))
 end
 
-# Treatment contrast matrices, not including intercept, for X
+# Treatment contrast matrices, not including intercept, for X (condition-Concentration categorical encoding)
 all_Xnoint = []
 for i in 1:length(all_X)
-  push!(all_Xnoint, convert(Array{Float64}, contr(all_X[i][[:Condition]], [:Condition], ["noint"])))
+  push!(all_Xnoint, convert(Array{Float64}, contr(all_X[i][[:Cond_Conc]], [:Cond_Conc], ["noint"])))
 end
 
-# Treatment contrast matrices, not including intercept or spatial effects, for Z
+# Treatment contrast matrices, not including intercept, for X (condition categorical encoding) 
+all_Xnoint_cond = []
+for i in 1:length(all_X)
+  push!(all_Xnoint_cond, convert(Array{Float64}, contr(all_X[i][[:Condition]], [:Condition], ["noint"])))
+end
+
+# Treatment contrast matrices, not including intercept, for Z
 all_Znoint = []
 for i in 1:length(all_Z)
   push!(all_Znoint, convert(Array{Float64}, contr(all_Z[i][:,[:name]], [:name], ["noint"])))
@@ -253,40 +312,29 @@ end
 
 # Run permutations for each plate and method.
 nperms = 1000
-for i in 1:6
+for i in 1:length(all_Ystd)
   println(string("Plate ", i))
 
-  # Run matrix linear models 
-  srand(50)
-  tic()
-  pvalst = perm_test(my_MLM, nperms, all_Ystd[i], all_Xdos[i], all_Zsumc[i],
-     string("./processed/KEIO_", i, "_dos.t.csv"))
-  toc()
+  # Dosage response
+  srand(1)
+  pvals_dos = convert(Array, perm_test(my_MLM_dos, nperms, all_Xdos[i], all_Ystd[i], all_Zsumc[i]))
 
-  # Run matrix linear models with variance shrinkage
-  srand(50)
-  tic()
-  pvalst_shrink = perm_test(my_MLM, nperms, all_Ystd[i], all_Xdos[i], all_Zsumc[i], 
-    string("./processed/KEIO_", i, "_dos.t_shrink.csv"), true)
-  toc()
+  # Categorical encoding of condition-concentrations for MLM and Collins
+  srand(1)
+  pvals = convert(Array, perm_test(my_MLM, nperms, all_Xsumc[i], all_Ystd[i], all_Zsumc[i]))
+  srand(1)
+  pvals_S = convert(Array, perm_test(my_Sscore_floor, nperms, all_Xnoint[i], all_Ystd[i], all_Znoint[i]))
 
-  # Run Collins S scores
-  srand(50)
-  tic()
-  pvalsS = perm_test(Sscore, nperms, all_Ystd[i], all_Xnoint[i], all_Znoint[i], 
-     string("./processed/KEIO_", i, "_dos.S.csv"))
-  toc()
-
-  # Run Collins S scores with variance bounds
-  srand(50)
-  tic()
-  pvalsS_floor = perm_test(Sscore_floor, nperms, all_Ystd[i], all_Xnoint[i], all_Znoint[i],
-     string("./processed/KEIO_", i, "_dos.S_floor.csv"))
-  toc()
+  # Categorical encoding of conditions for MLM and Collins
+  srand(1)
+  pvals_cond = convert(Array, perm_test(my_MLM, nperms, all_Xsumc_cond[i], all_Ystd[i], all_Zsumc[i]))
+  srand(1)
+  pvals_S_cond = convert(Array, perm_test(my_Sscore_floor, nperms, all_Xnoint_cond[i], all_Ystd[i], all_Znoint[i]))
 
   # Write p-values to CSV.
-  writecsv(string("./processed/KEIO_", i, "_dos.pvalst.csv"), pvalst)  
-  writecsv(string("./processed/KEIO_", i, "_dos.pvalst_shrink.csv"), pvalst_shrink)
-  writecsv(string("./processed/KEIO_", i, "_dos.pvalsS.csv"), pvalsS)
-  writecsv(string("./processed/KEIO_", i, "_dos.pvalsS_floor.csv"), pvalsS_floor)
+  writecsv(string("./processed/dosage_keio_p", i, "_pvals_dos.csv"), pvals_dos)
+  writecsv(string("./processed/dosage_keio_p", i, "_pvals.csv"), pvals)
+  writecsv(string("./processed/dosage_keio_p", i, "_pvals_S.csv"), pvals_S)
+  writecsv(string("./processed/dosage_keio_p", i, "_pvals_cond.csv"), pvals_cond)
+  writecsv(string("./processed/dosage_keio_p", i, "_pvals_S_cond.csv"), pvals_S_cond)
 end
